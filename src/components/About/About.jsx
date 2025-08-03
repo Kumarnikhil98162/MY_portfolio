e91cf4c985b3a821d77f877841cae3b94a9e8bd4
Nikhil <128123533+Kumarnikhil98162@users.noreply.github.com>
2025-08-03 13:23:20 +0530

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Typewriter } from 'react-simple-typewriter';
 import Tilt from 'react-parallax-tilt';
-import profileImage from '../../assets/profile1.jpeg';
+import profileImage from '../../assets/profile.jpeg';
 function About() {
   return (
     <section
